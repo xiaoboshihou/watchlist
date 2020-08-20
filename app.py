652a906e26dbec7dae2a4994eebb7d8a2b54121d
xiaoboshi <lixueyuan1990@outlook.com>
2020-08-20 21:39:11 +0800

    Add minimal home page

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, url_for, escape
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello!'
+
+
+@app.route('/home')
+def home():
+    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+
+
+@app.route('/user/<name>')
+def user_page(name):
+    return 'User: %s' % escape(name)
+
+
+@app.route('/test')
+def test_url_for():
+    print(url_for('hello'))
+    print(url_for('user_page', name='lxy'))
+    print(url_for('user_page', name='herman'))
+    print(url_for('test_url_for'))
+    print(url_for('test_url_for', num=2))
+    return 'Test page'
+
+
+if __name__ == '__main__':
+    app.run()
