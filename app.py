cf93a3dcc9ad130d81619f35095491be21b06627
xiaoboshi <lixueyuan1990@outlook.com>
2020-08-25 23:06:16 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,14 +1,47 @@
+import os
+import sys
+import click
 from flask import Flask, url_for, escape, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
-@app.route('/')
-def hello():
-    return 'Hello!'
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
 
 
-@app.route('/index')
-def index():
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+
+@app.cli.command()    # 注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop.')
+def initdb(drop):
+    """Initialize the database."""
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')    # 输出提示信息
+
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+
     name = 'lxy'
     movies = [{'title': 'My Neighbor Totoro', 'year': '1988'},
               {'title': 'Dead Poets Society', 'year': '1989'},
@@ -20,7 +53,25 @@ def index():
               {'title': 'Devils on the Doorstep', 'year': '1999'},
               {'title': 'WALL-E', 'year': '2008'},
               {'title': 'The Pork of Music', 'year': '2012'}]
-    return render_template('index.html', name=name, movies=movies)
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+    db.session.commit()
+    click.echo('Done.')
+
+
+@app.route('/')
+def hello():
+    return 'Hello!'
+
+
+@app.route('/index')
+def index():
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html', username=user, movies=movies)
 
 
 @app.route('/home')
