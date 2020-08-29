7dc8318bf5c5436d1cccc7f930e465ec1e1a1fd7
xiaoboshi <lixueyuan1990@outlook.com>
2020-08-29 23:12:20 +0800

    Add base tt tetemplate and error template

--- app.py
+++ app.py
@@ -62,6 +62,17 @@ def forge():
     click.echo('Done.')
 
 
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 @app.route('/')
 def hello():
     return 'Hello!'
@@ -69,9 +80,8 @@ def hello():
 
 @app.route('/index')
 def index():
-    user = User.query.first()
     movies = Movie.query.all()
-    return render_template('index.html', username=user, movies=movies)
+    return render_template('index.html', movies=movies)
 
 
 @app.route('/home')
